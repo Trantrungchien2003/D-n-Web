c696f09bfcaff302c58d7ddfba34365ce00429f6
Thang220173 <thang.220173@tbd.edu.vn>
2024-10-31 14:21:52 +0700

    tim san pham theo thuong hieu hoac theo danh muc

--- Shopping_Tutorial/Controllers/CategoryController.cs
+++ Shopping_Tutorial/Controllers/CategoryController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping_Tutorial.Models;
+using Shopping_Tutorial.Repository;
 
 namespace Shopping_Tutorial.Controllers
 {
 	public class CategoryController : Controller
 	{
-		public IActionResult Index()
+		private readonly DataContext _dataContext;
+		public CategoryController(DataContext context)
 		{
-			return View();
+			_dataContext = context;
+		}
+		public async Task<IActionResult> Index(string slug="")
+		{
+			CategoryModel category = _dataContext.Categories.Where(c => c.slug == slug).FirstOrDefault();
+
+			if (category == null) return RedirectToAction("Index");
+
+			var productsByCategory = _dataContext.Products.Where(c => c.CategoryId == category.Id);
+
+			return View(await productsByCategory.OrderByDescending(c => c.Id).ToListAsync());
 		}
 	}
 }
