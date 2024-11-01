df449b6e40350cc8447e1575665ec72ef7278e2f
TQT279 <tqt158@gmail.com>
2024-11-01 12:33:50 +0700

    View-for-cart

--- Shopping_Tutorial/Controllers/CartController.cs
+++ Shopping_Tutorial/Controllers/CartController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shopping_Tutorial.Models;
+using Shopping_Tutorial.Models.ViewModels;
+using Shopping_Tutorial.Repository;
 
 namespace Shopping_Tutorial.Controllers
 {
     public class CartController : Controller
     {
+		private readonly DataContext _dataContext;
+		public CartController(DataContext _context)
+		{
+			_dataContext = _context;
+		}
 		public IActionResult Index()
 		{
-			return View();
+			List<CartItemModel> cartItems = HttpContext.Session.GetJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
+            CartItemViewModel cartVM = new()
+			{ 
+				CartItems = cartItems,
+				GrandTotal = cartItems.Sum(x => x.Quantity * x.Price)
+			};
+			return View(cartVM);
 		}
 		public IActionResult Checkout()
 		{
