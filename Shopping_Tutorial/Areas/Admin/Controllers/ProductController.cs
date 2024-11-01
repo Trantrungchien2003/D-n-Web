e0d646f5bfd70236f575d6994e6acf1b3aff0722
Thang220173 <thang.220173@tbd.edu.vn>
2024-11-01 14:35:51 +0700

    Trang Admin

---
+++ Shopping_Tutorial/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopping_Tutorial.Repository;
+
+namespace Shopping_Tutorial.Area.Admin.Controllers
+{
+	[Area("Admin")]
+    public class ProductController : Controller
+    {
+		private readonly DataContext _dataContext;
+		public ProductController(DataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+		public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Products.OrderByDescending(p => p.Id).Include(p => p.Category).Include(p => p.Brand).ToListAsync());
+        }
+    }
+}
