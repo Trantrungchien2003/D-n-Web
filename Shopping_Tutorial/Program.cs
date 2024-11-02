719b4ae4aa9b92154270eb82b70aa4ba0a00e1fe
Trantrungchien2003 <abcd04022004@gmail.com>
2024-11-03 00:11:35 +0700

    bai31 404

--- Shopping_Tutorial/Program.cs
+++ Shopping_Tutorial/Program.cs
@@ -22,7 +22,13 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+
+
+
 var app = builder.Build();
+app.UseStatusCodePagesWithRedirects("/Home/Error?statuscode={0}");
+
+
 
 app.UseSession();
 
@@ -40,6 +46,19 @@ app.UseAuthorization();
 app.MapControllerRoute(
 	name: "Areas",
 	pattern: "{area:exists}/{controller=Product}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "category",
+    pattern: "/category/{Slug?}",  
+    defaults : new { controller = "Category", action = "Index" });
+
+app.MapControllerRoute(
+    name: "brand",
+    pattern: "/brand/{Slug?}",
+    defaults: new { controller = "Brand", action = "Index" });
+
+
+
 
 app.MapControllerRoute(
     name: "default",
